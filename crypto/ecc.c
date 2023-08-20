a99b4133db9c6c333276bbc4d8d1c3f18c1bf591
Eric Cox <contact@ericcox.me>
2023-08-19 21:42:04 -0400

    Fix Elliptic Curves for Linux for Tegra Kernel 5.10
    While merging L4T 4.9 with 5.10, modifications to the elliptic curves
    were dropped. This commit restores dropped curve parameters to match
    upstream parameters, namely by include a and b parameters for both the
    NIST P-192 and P-256 curves.
    
    Without these, bluetooth LE crashes the kernel when a connection is
    attempted, such as reported in the following NVidia support requests:
    https://forums.developer.nvidia.com/t/225752
    https://forums.developer.nvidia.com/t/253849
    https://forums.developer.nvidia.com/t/257345

--- crypto/ecc.c
+++ crypto/ecc.c
@@ -50,6 +50,10 @@ static u64 nist_p192_p[] = { 0xFFFFFFFFFFFFFFFFull, 0xFFFFFFFFFFFFFFFEull,
 				0xFFFFFFFFFFFFFFFFull };
 static u64 nist_p192_n[] = { 0x146BC9B1B4D22831ull, 0xFFFFFFFF99DEF836ull,
 				0xFFFFFFFFFFFFFFFFull };
+static u64 nist_p192_a[] = { 0xFFFFFFFFFFFFFFFCull, 0xFFFFFFFFFFFFFFFEull,
+                               0xFFFFFFFFFFFFFFFFull };
+static u64 nist_p192_b[] = { 0xFEB8DEECC146B9B1ull, 0x0FA7E9AB72243049ull,
+                               0x64210519E59C80E7ull };
 static struct ecc_curve nist_p192 = {
 	.name = "nist_192",
 	.g = {
@@ -58,7 +62,9 @@ static struct ecc_curve nist_p192 = {
 		.ndigits = 3,
 	},
 	.p = nist_p192_p,
-	.n = nist_p192_n
+	.n = nist_p192_n,
+        .a = nist_p192_a,
+        .b = nist_p192_b
 };
 
 /* NIST P-256 */
@@ -70,6 +76,10 @@ static u64 nist_p256_p[] = { 0xFFFFFFFFFFFFFFFFull, 0x00000000FFFFFFFFull,
 				0x0000000000000000ull, 0xFFFFFFFF00000001ull };
 static u64 nist_p256_n[] = { 0xF3B9CAC2FC632551ull, 0xBCE6FAADA7179E84ull,
 				0xFFFFFFFFFFFFFFFFull, 0xFFFFFFFF00000000ull };
+static u64 nist_p256_a[] = { 0xFFFFFFFFFFFFFFFCull, 0x00000000FFFFFFFFull,
+                               0x0000000000000000ull, 0xFFFFFFFF00000001ull };
+static u64 nist_p256_b[] = { 0x3BCE3C3E27D2604Bull, 0x651D06B0CC53B0F6ull,
+                               0xB3EBBD55769886BCull, 0x5AC635D8AA3A93E7ull };
 static struct ecc_curve nist_p256 = {
 	.name = "nist_256",
 	.g = {
@@ -78,7 +88,9 @@ static struct ecc_curve nist_p256 = {
 		.ndigits = 4,
 	},
 	.p = nist_p256_p,
-	.n = nist_p256_n
+	.n = nist_p256_n,
+	.n = nist_p256_a,
+	.n = nist_p256_b
 };
 
 /* BrainPool P-256 */
